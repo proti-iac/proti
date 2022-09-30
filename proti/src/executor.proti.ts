b0c5ab899a4fee2e31c5e004d012aa92e2ce646a
Daniel Sokolowski <github@d.sokolowski.org>
2022-09-30 16:12:39 +0200

    Use module preloader in executor

--- proti/src/executor.proti.ts
+++ proti/src/executor.proti.ts
@@ -1,24 +1,36 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as fc from 'fast-check';
-import { Map } from 'immutable';
-import * as path from 'path';
-import { Config } from './config';
+import { Map, Set } from 'immutable';
+import { Config, fromPrimitiveConfig, PrimitiveConfig } from './config';
+import { Preloader } from './module-preloader';
 
-declare const proti: Config;
+declare const proti: PrimitiveConfig;
+const config: Config = fromPrimitiveConfig(proti);
+
+const showModules = (headline: string, modules: Set<string>): void =>
+	console.info(`${headline} (${modules.size}):\n${modules.join('\n')}`);
 
 describe(proti.projectDir, () => {
-	const dynamicallyLoadedModules: string[] = [];
+	const dynamicallyLoadedModules: Set<string>[] = [];
 
 	beforeAll(async () => {
+		const preloader = new Preloader(config);
+		const preloadedModules = await preloader.preloadModules();
+		if (proti.showPreloadedImports)
+			showModules(
+				'Preloaded modules that are shared among all test executions',
+				preloadedModules.keySeq().toSet()
+			);
+
 		// We need to mutate the same runtime instance that is later used by the Pulumi project
-		const projPulumiPath = require.resolve('@pulumi/pulumi', { paths: [proti.projectDir] });
-		const projectPulumi: typeof pulumi = await import(projPulumiPath);
+		const pulumiPath = preloader.resolveInProject('@pulumi/pulumi');
+		const projectPulumi: typeof pulumi = preloadedModules.get(pulumiPath);
 
 		projectPulumi.runtime.setMocks({
 			newResource(args: pulumi.runtime.MockResourceArgs): { id: string; state: any } {
 				return {
 					id: `${args.inputs.name}_id`,
-					state: args.inputs,
+					state: { ...args.inputs, versioning: { enabled: true }, bucket: null },
 				};
 			},
 			call(args: pulumi.runtime.MockCallArgs) {
@@ -30,7 +42,7 @@ describe(proti.projectDir, () => {
 		// the mock constants' initialization.
 		// eslint-disable-next-line no-lone-blocks
 		{
-			jest.mock(projPulumiPath, () => projectPulumi);
+			preloadedModules.forEach((mod, modPath) => jest.mock(modPath, () => mod));
 		}
 	});
 
@@ -38,18 +50,19 @@ describe(proti.projectDir, () => {
 		fc.property(fc.string(), (testName) => {
 			it(`should run ${testName}`, async () => {
 				jest.isolateModules(() => {
+					// Run the IaC program
 					// eslint-disable-next-line import/no-dynamic-require, global-require
 					require(proti.projectDir);
 
 					// Evaluate which modules have been dynamically loaded for this single test execution
-					const loadedModules: string[] = [
+					const loadedModules: Set<string> = Set([
 						...(jest.getIsolatedModuleRegistry() || Map<string, any>()).keys(),
-					];
+					]);
 					expect(
 						loadedModules,
 						"Isolated module registry does not contain Pulumi program's main module. The tests do not execute the IaC program!"
-					).toContain(require.resolve(proti.projectDir));
-					if (proti.showDynamicImports) dynamicallyLoadedModules.push(...loadedModules);
+					).toContain(require.resolve(config.projectDir));
+					if (proti.showDynamicImports) dynamicallyLoadedModules.push(loadedModules);
 				});
 
 				// Wait for all async code to settle
@@ -60,17 +73,19 @@ describe(proti.projectDir, () => {
 	);
 
 	afterAll(() => {
-		if (proti.showDynamicImports && !proti.silent) {
-			const moduleStats = dynamicallyLoadedModules
-				.reduce(
-					(stats, mod) => stats.set(mod, (stats.get(mod) || 0) + 1),
-					Map<string, number>()
-				)
-				.toArray()
-				.sort()
-				.map(([mod, tests]) => `${tests.toString().padStart(5, ' ')} ${mod}`)
-				.join('\n');
-			console.info(`Dynamically loaded modules [test runs, module name]:\n${moduleStats}`);
+		if (proti.showDynamicImports) {
+			const padLength = Math.log10(dynamicallyLoadedModules.length) + 1;
+			showModules(
+				'Dynamically loaded modules [test runs: module]',
+				dynamicallyLoadedModules
+					.reduce((acc, run) => acc.concat(run.toArray()), [] as string[])
+					.reduce(
+						(stat, mod) => stat.set(mod, (stat.get(mod) || 0) + 1),
+						Map<string, number>()
+					)
+					.map((numTests, mod) => `${numTests.toString().padStart(padLength)}: ${mod}`)
+					.toSet()
+			);
 		}
 	});
 });
