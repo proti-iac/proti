48e54542aa9376e4b490859ec437223c24bcadb5
Daniel Sokolowski <github@d.sokolowski.org>
2023-04-24 20:01:26 +0200

    Fix test-runner dependencies

--- proti/proti-test-runner/src/jest-preset.ts
+++ proti/proti-test-runner/src/jest-preset.ts
@@ -23,6 +23,10 @@ const config: JestConfigWithTsJest = {
 	moduleFileExtensions: [...defaults.moduleFileExtensions, 'yaml', 'yml'],
 	runner: '@proti/runner',
 	// seed: 0, // Tests' rng seed. Set to a value for deterministic reruns.
+	// reporters: [
+	// 	'default', // Jest's default console reporting
+	// 	'@proti/reporter', // ProTI's CSV reporter
+	// ],
 	testEnvironment: 'node',
 	testMatch: ['**/Pulumi.y?(a)ml'],
 	testRunner: '@proti/test-runner',
