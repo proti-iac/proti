93edcf27e1ece3db75cd59d7d7c9a226652d217a
Daniel Sokolowski <github@d.sokolowski.org>
2023-05-02 13:53:32 +0200

    Fix random-word-webpage example as shown in paper

--- examples/random-word-webpage/paper/index.ts
+++ examples/random-word-webpage/paper/index.ts
@@ -17,7 +17,15 @@ rng.result.apply((wordId) => {
     });
 });
 
-
+// Set the public access policy requires updating ownership controls and disabling block public access since May 2023 
+const ownershipControls = new aws.s3.BucketOwnershipControls("ownership-controls", {
+    bucket: bucket.id,
+    rule: { objectOwnership: "ObjectWriter" }
+});
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
+    bucket: bucket.id,
+    blockPublicAcls: false,
+});
 // Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy('bucketPolicy', {
 	bucket: bucket.bucket, // Direct dependency
@@ -33,6 +41,5 @@ const bucketPolicy = new aws.s3.BucketPolicy('bucketPolicy', {
 		],
 	},
 });
-
 
 export const url = bucket.websiteEndpoint;
