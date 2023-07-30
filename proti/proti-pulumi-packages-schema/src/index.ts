fe001f6e23b80387d1de29db474fadcc1864e539
Daniel Sokolowski <github@d.sokolowski.org>
2023-07-30 18:03:14 +0200

    Export arbitrary and oracle types

--- proti/proti-pulumi-packages-schema/src/index.ts
+++ proti/proti-pulumi-packages-schema/src/index.ts
@@ -1,4 +1,12 @@
+export {
+	Arbitrary,
+	ArbitraryCache,
+	PulumiPackagesSchemaArbitrary,
+	PulumiPackagesSchemaArbitraryContext,
+	PulumiPackagesSchemaGenerator,
+} from './arbitrary';
 export * from './config';
+export { PulumiPackagesSchemaOracle, Validator, ValidatorCache } from './oracle';
 export * from './pulumi';
 export * from './schema-registry';
 export * from './utils';
