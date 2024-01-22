eef23283408b9a73baee3b289520016687893e1a
Daniel Sokolowski <github@d.sokolowski.org>
2024-01-22 19:33:47 +0100

    Fix resolution of relative project main paths in Pulumi.yaml (#11)

--- proti-core/test/pulumi-project.test.ts
+++ proti-core/test/pulumi-project.test.ts
@@ -3,35 +3,33 @@ import * as path from 'path';
 import { PulumiProject, readPulumiProject } from '../src/pulumi-project';
 
 describe('pulumi project', () => {
-	const validProjects: (Omit<PulumiProject, 'main'> & Partial<PulumiProject>)[] = [
-		{ projectFile: 'pulumi-project/Pulumi.yaml' },
+	/**
+	 * Expected project configs for Pulumi project YAML files in the ./pulumi-project/ subdirectory
+	 */
+	const validProjects: PulumiProject[] = [
 		{
-			projectFile: 'pulumi-project/Pulumi2.yaml',
+			projectFile: path.resolve(__dirname, 'pulumi-project/Pulumi.yaml'),
+			main: path.resolve(__dirname, 'pulumi-project'),
+		},
+		{
+			projectFile: path.resolve(__dirname, 'pulumi-project/Pulumi2.yaml'),
+			main: path.resolve(__dirname, 'pulumi-project'),
 			name: 'test',
 			description: 'A minimal AWS TypeScript Pulumi program',
 		},
 		{
-			projectFile: 'pulumi-project/Pulumi3.yaml',
+			projectFile: path.resolve(__dirname, 'pulumi-project/Pulumi3.yaml'),
 			main: '/hello/world.ts',
 			name: 'test',
 			description: 'A minimal AWS TypeScript Pulumi program',
 		},
+		{
+			projectFile: path.resolve(__dirname, 'pulumi-project/Pulumi4.yaml'),
+			main: path.resolve(__dirname, 'pulumi-project/hello/world.ts'),
+		},
 	];
-
-	it.each(
-		validProjects.map((partialProject) => {
-			const projectFile = path.resolve(__dirname, partialProject.projectFile);
-			return [
-				projectFile,
-				{
-					...partialProject,
-					projectFile,
-					main: partialProject.main || path.dirname(projectFile),
-				},
-			];
-		})
-	)('should load %s', (projectFile, project) =>
-		expect(readPulumiProject(projectFile)).resolves.toStrictEqual(project)
+	it.each(validProjects)('should load %s', (project) =>
+		expect(readPulumiProject(project.projectFile)).resolves.toStrictEqual(project)
 	);
 
 	it('should throw on invalid path', () => {
