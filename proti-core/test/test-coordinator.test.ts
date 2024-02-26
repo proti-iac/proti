2447fd029f02f76731db44f10ce6c1aa8677d3eb
Daniel Sokolowski <github@d.sokolowski.org>
2024-02-26 18:15:52 +0100

    Add dedicated test phase for ProTI plugin loading (#14)

--- proti-core/test/test-coordinator.test.ts
+++ proti-core/test/test-coordinator.test.ts
@@ -15,14 +15,13 @@ describe('test coordinator', () => {
 	};
 
 	describe('loading test oracles', () => {
-		const coordinatorForOracles = (oracles: string[]): TestCoordinator =>
-			new TestCoordinator({ ...defaultTestCoordinatorConfig(), oracles }, testModuleConfig);
-
 		it('should not load oracles', async () => {
-			const coordinator = coordinatorForOracles([]);
-			Object.entries(await coordinator.oracles).forEach(([, oracles]) =>
-				expect(oracles.length).toBe(0)
-			);
+			Object.entries(
+				await TestCoordinator.loadOracles(
+					{ ...defaultTestCoordinatorConfig(), oracles: [] },
+					testModuleConfig
+				)
+			).forEach(([, oracles]) => expect(oracles.length).toBe(0));
 		});
 
 		type OracleTypes = 'resource' | 'asyncResource' | 'deployment' | 'asyncDeployment';
@@ -33,57 +32,70 @@ describe('test coordinator', () => {
 			['async deployment', ['async-deployment-oracle'], ['asyncDeployment']],
 			['combined', ['combined-oracle'], ['resource', 'asyncDeployment']],
 		])('should load %s oracle', async (_, files, types) => {
-			const coordinator = coordinatorForOracles(
-				files.map((file) => path.resolve(__dirname, './test-coordinator-tests/', file))
+			const oracles = await TestCoordinator.loadOracles(
+				{
+					...defaultTestCoordinatorConfig(),
+					oracles: files.map((file) =>
+						path.resolve(__dirname, './test-coordinator-tests/', file)
+					),
+				},
+				testModuleConfig
 			);
-			Object.entries(await coordinator.oracles).forEach(([type, oracles]) => {
-				expect(oracles.length).toBe(types.includes(type as OracleTypes) ? 1 : 0);
-				oracles.forEach((oracle: unknown) => expect(isOracle(oracle)).toBe(true));
+			Object.entries(oracles).forEach(([type, orcls]) => {
+				expect(orcls.length).toBe(types.includes(type as OracleTypes) ? 1 : 0);
+				orcls.forEach((oracle: unknown) => expect(isOracle(oracle)).toBe(true));
 			});
 		});
 
-		it('should fail on loading non-existing oracle', () =>
-			expect(() => coordinatorForOracles(['a']).oracles).rejects.toThrow(
-				/Cannot find module 'a' from /
-			));
+		it('should fail on loading non-existing oracle', () => {
+			const oracles = TestCoordinator.loadOracles(
+				{ ...defaultTestCoordinatorConfig(), oracles: ['a'] },
+				testModuleConfig
+			);
+			expect(oracles).rejects.toThrow(/Cannot find module 'a' from /);
+		});
 
 		it('should init loaded oracle module', async () => {
 			const initOraclePath = path.resolve(__dirname, './test-coordinator-tests/oracle-init');
 			// eslint-disable-next-line import/no-dynamic-require, global-require
 			const module = require(initOraclePath);
 			expect(module.config).toBe(undefined);
-			await coordinatorForOracles([initOraclePath]).oracles;
+			await TestCoordinator.loadOracles(
+				{ ...defaultTestCoordinatorConfig(), oracles: [initOraclePath] },
+				testModuleConfig
+			);
 			expect(module.config).toBe(testModuleConfig);
 		});
 	});
 
 	describe('loading generator arbitrary', () => {
-		const coordinatorForArbitrary = (arbitrary: string): TestCoordinator =>
-			new TestCoordinator(
-				{
-					...defaultTestCoordinatorConfig(),
-					arbitrary,
-				},
-				testModuleConfig
-			);
 		const arbPath = path.resolve(__dirname, './test-coordinator-tests/arbitrary');
 		const initArbPath = path.resolve(__dirname, './test-coordinator-tests/arbitrary-init');
 
 		it('should load arbitrary', async () => {
-			const coordinator = coordinatorForArbitrary(arbPath);
-			expect(is<fc.Arbitrary<Generator>>(await coordinator.arbitrary)).toBe(true);
+			const arb = await TestCoordinator.loadArbitrary(
+				{ ...defaultTestCoordinatorConfig(), arbitrary: arbPath },
+				testModuleConfig
+			);
+			expect(is<fc.Arbitrary<Generator>>(arb)).toBe(true);
 		});
 
-		it('should fail on loading non-existing arbitrary', () =>
-			expect(() => coordinatorForArbitrary('a').arbitrary).rejects.toThrow(
-				/Cannot find module 'a' from /
-			));
+		it('should fail on loading non-existing arbitrary', () => {
+			const arb = TestCoordinator.loadArbitrary(
+				{ ...defaultTestCoordinatorConfig(), arbitrary: 'a' },
+				testModuleConfig
+			);
+			expect(arb).rejects.toThrow(/Cannot find module 'a' from /);
+		});
 
 		it('should init loaded arbitrary module', async () => {
 			// eslint-disable-next-line import/no-dynamic-require, global-require
 			const module = require(initArbPath);
 			expect(module.config).toBe(undefined);
-			await coordinatorForArbitrary(initArbPath).arbitrary;
+			await TestCoordinator.loadArbitrary(
+				{ ...defaultTestCoordinatorConfig(), arbitrary: initArbPath },
+				testModuleConfig
+			);
 			expect(module.config).toBe(testModuleConfig);
 		});
 	});
