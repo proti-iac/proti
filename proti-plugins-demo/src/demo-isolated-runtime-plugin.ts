5ff04d720f6314493c834d7b2bdf6870df14d698
Daniel Sokolowski <github@d.sokolowski.org>
2024-05-10 11:58:11 +0200

    Add isolated jest runtime demo

--- proti-plugins-demo/src/demo-isolated-runtime-plugin.ts
+++ proti-plugins-demo/src/demo-isolated-runtime-plugin.ts
@@ -1,17 +1,70 @@
-import type { ResourceOracle, TestResult } from '@proti-iac/core';
+import { type ShouldInstrumentOptions, createScriptTransformer } from '@jest/transform';
+import type { Config } from '@jest/types';
+import {
+	ModuleLoader,
+	PluginArgs,
+	type PluginWithInitFn,
+	type ResourceOracle,
+	type TestResult,
+} from '@proti-iac/core';
+import Runtime from 'jest-runtime';
 
 /**
- * Simple {@link ResourceOracle} emonstrating a ProTI plugin with an isolated Jest runtime.
+ * Simple {@link ResourceOracle} demonstrating a ProTI plugin with an isolated Jest runtime.
  */
-export class DemoIsolatedRuntimePlugin implements ResourceOracle<null> {
-	name = 'Demo Isolated Runtime';
+export class DemoIsolatedRuntimePlugin implements ResourceOracle<null>, PluginWithInitFn {
+	readonly name = 'Demo Isolated Runtime';
 
-	description = 'An oracle plugin demonstrating a ProTI plugin with an isolated Jest runtime';
-
-	// eslint-disable-next-line class-methods-use-this
-	newRunState = () => null;
+	readonly description =
+		'An oracle plugin demonstrating a ProTI plugin with an isolated Jest runtime';
 
 	// eslint-disable-next-line class-methods-use-this
-	validateResource = (): TestResult => undefined;
+	readonly newRunState = () => null;
+
+	// eslint-disable-next-line class-methods-use-this
+	readonly validateResource = (): TestResult => undefined;
+
+	// eslint-disable-next-line class-methods-use-this
+	readonly init = async ({
+		globalConfig,
+		projectConfig,
+		environment,
+		resolver,
+		testPath,
+		hasteFS,
+		protiConfig,
+		pulumiProject,
+	}: PluginArgs) => {
+		const cacheFS = new Map();
+		const coverageOptions: ShouldInstrumentOptions = {
+			collectCoverage: globalConfig.collectCoverage,
+			collectCoverageFrom: globalConfig.collectCoverageFrom as string[],
+			coverageProvider: globalConfig.coverageProvider,
+		};
+		const transformer = await createScriptTransformer(
+			projectConfig as Config.ProjectConfig,
+			cacheFS
+		);
+		const runtime = new Runtime(
+			projectConfig as Config.ProjectConfig,
+			environment,
+			resolver,
+			transformer,
+			cacheFS,
+			coverageOptions,
+			testPath,
+			globalConfig as Config.GlobalConfig
+		);
+		const modLoader = await ModuleLoader.create(
+			projectConfig,
+			protiConfig.moduleLoading,
+			runtime,
+			resolver,
+			hasteFS,
+			pulumiProject.main
+		);
+		// Run program in isolated environment
+		await modLoader.execProgram();
+	};
 }
 export default DemoIsolatedRuntimePlugin;
